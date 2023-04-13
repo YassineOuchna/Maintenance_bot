f5631b411e89fe8e292ac9d9f9c94171aeee8343
Yassine Ouchna <yassine.ouchna@student-cs.fr>
2023-04-13 22:41:18 +0200

    adding_rows

--- bot.py
+++ bot.py
@@ -1,26 +1,38 @@
 import sqlite3 as sq
 
 conn = sq.connect('database.db')
+cur = conn.cursor()
 
 
-class bot:
-    def __init__(self) -> None:
-        pass
+class logs:  # le tableau des maintenances
+    def __init__(self) -> None:  # creation de la table des maintenances
+        cur.execute(
+            "CREATE TABLE IF NOT EXISTS maintenances(id int NOT NULL, name , type, date_of_maintenance date,length int,owner,members,risk_lvl int,risk_cmt DEFAULT '',comment DEFAULT '',tags DEFAULT '')")
 
-    def add(self, name, type, date, length, members, risk_lvl, risk_cmt, comment, tags, owner):  # ajout d'une maintenance
-        pass
+    def end(self):
+        conn.close()
 
-    def get(self, name):
-        pass
+    def add(name, type, date, length, owner, members, risk_lvl, risk_cmt='', comment='', tags=''):  # ajout d'une maintenance
+        new_id = cur.execute(
+            "SELECT max(id) from maintenances").fetchone()[0] + 1
+        cur.execute(
+            f"INSERT INTO maintenances (id, name) VALUES ({new_id}, '{name}')")
+        conn.commit()
 
-    def edit(self, name, *what_to_edit):
-        pass
+    def get(name):
+        conn.commit()
+
+    def edit(name, *edits):
+        conn.commit()
 
     def add_user(self, name, user):
-        pass
+        conn.commit()
 
-    def del_user(self, name, user):
-        pass
+    def del_user(name, user):
+        conn.commit()
 
-    def delete(self, name):
-        pass
+    def delete(name):
+        conn.commit()
+
+
+logs.add('test_row', 'test_ugh', '13-04-2023', 0.5, 'me', 'me', 0, 0)
