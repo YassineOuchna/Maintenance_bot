dc11d4a8ba97873e289e88a1c4482b3a85a7ed08
Yassine Ouchna <yassine.ouchna@student-cs.fr>
2023-04-14 14:50:10 +0200

    delete function

--- bot.py
+++ bot.py
@@ -7,22 +7,23 @@ cur = conn.cursor()
 class logs:  # le tableau des maintenances
     def __init__(self) -> None:  # creation de la table des maintenances
         cur.execute(
-            "CREATE TABLE IF NOT EXISTS maintenances(id int NOT NULL, name , type, date_of_maintenance date,length int,owner,members,risk_lvl int,risk_cmt DEFAULT '',comment DEFAULT '',tags DEFAULT '')")
+            "CREATE TABLE IF NOT EXISTS maintenances(id int NOT NULL, name , type, date_of_maintenance date,length int,owner,members DEFAULT '',risk_level int,risk_comment DEFAULT '',comment DEFAULT '',tags DEFAULT '')")
 
-    def end(self):
+    def end(self):  # Closes connection
         conn.close()
 
     def add(name, type, date, length, owner, members, risk_lvl, risk_cmt='', comment='', tags=''):  # ajout d'une maintenance
         new_id = cur.execute(
             "SELECT max(id) from maintenances").fetchone()[0] + 1
         cur.execute(
-            f"INSERT INTO maintenances (id, name) VALUES ({new_id}, '{name}')")
+            f"INSERT INTO maintenances VALUES ({new_id}, '{name}', '{type}','{date}',{length}, '{owner}', '{members}', {risk_lvl}, '{risk_cmt}','{comment}','{tags}')")
         conn.commit()
 
     def get(name):
         conn.commit()
 
     def edit(name, *edits):
+        conn.execute()
         conn.commit()
 
     def add_user(self, name, user):
@@ -31,8 +32,6 @@ class logs:  # le tableau des maintenances
     def del_user(name, user):
         conn.commit()
 
-    def delete(name):
+    def delete(id):
+        conn.execute(f'DELETE FROM maintenances WHERE id={id}')
         conn.commit()
-
-
-logs.add('test_row', 'test_ugh', '13-04-2023', 0.5, 'me', 'me', 0, 0)
