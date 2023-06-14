3851dd82f7bc13c16752cc24fa6d879aa9bd4a6d
Yassine Ouchna <yassine.ouchna@student-cs.fr>
2023-06-14 02:17:06 +0200

    bot_can_add_rows

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
 from telegram import Update
-from telegram.ext import ApplicationBuilder, CommandHandler, ContextTypes, ConversationHandler, MessageHandler
+from telegram.ext import ApplicationBuilder, CommandHandler, ContextTypes, ConversationHandler, MessageHandler, filters
 from bot import logs
+
+ADD_NAME, ADD_TYPE, ADD_DATE, ADD_LENGHT, ADD_MEMBERS, ADD_RISK, ADD_RCMT, ADD_CMT, ADD_TAGS = range(
+    9)
 
 
 async def hello(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
@@ -8,19 +11,64 @@ async def hello(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
 
 
 async def help(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
-    await update.message.reply_text("I can help you manage all your maintenance needs through these commands : \n **adding a maintenance** ")
+    await update.message.reply_text("I can help you manage all your maintenance needs through these commands : \n"
+                                    "-> add : adding a maintenance \n"
+                                    "-> past [int] : show the last [int] maintenances \n"
+                                    "-> get : show a specific maintenance \n"
+                                    "-> edit : modify a maintenance")
 
 
 async def add(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
     await update.message.reply_text("Choose a name for your maintenance")
-    return 0
+    global maintenance
+    maintenance = []
+    return ADD_NAME
 
 
-def add_name(update, context):
+async def add_name(update, context: ContextTypes.DEFAULT_TYPE):
     name = update.message.text
-    update.message.reply_text(f'great! you chose {name}')
-    return ConversationHandler.END
+    maintenance.append(name)
+    await update.message.reply_text('Great! Now specify the type of the maintenance')
+    return ADD_TYPE
 
+
+async def add_type(update, context: ContextTypes.DEFAULT_TYPE):
+    type = update.message.text
+    maintenance.append(type)
+    await update.message.reply_text('Specify a date for the maintenance in the formate d-m-y')
+    return ADD_DATE
+
+
+async def add_date(update, context=ContextTypes.DEFAULT_TYPE):
+    date = update.message.text
+    maintenance.append(date)
+    await update.message.reply_text('How many hours will it take ?')
+    return ADD_LENGHT
+
+
+async def add_lenght(update, context=ContextTypes.DEFAULT_TYPE):
+    lenght = update.message.text
+    maintenance.append(lenght)
+    maintenance.append(update.message.from_user.username)
+    await update.message.reply_text('Other than you, who else will help with this maintenance ? Don\'t forget to use - as a seperator')
+    return ADD_MEMBERS
+
+
+async def add_members(update, context=ContextTypes.DEFAULT_TYPE):
+    members = update.message.text
+    # adding the username as the default owner of the maintenance
+    maintenance.append(members)
+    await update.message.reply_text('Rate the risk of the maintenance from 0 to 5')
+    return ADD_RISK
+
+
+async def add_risk(update, context=ContextTypes.DEFAULT_TYPE):
+    risk = update.message.text
+    maintenance.append(risk)
+    await update.message.reply_text('Great! Now you can add a risk comment or /skip if you don\'t want to')
+    logs.add(maintenance[0], maintenance[1], maintenance[2],
+             maintenance[3], maintenance[4], maintenance[5], maintenance[6])
+    return ConversationHandler.END
 
 app = ApplicationBuilder().token(
     "5909711949:AAHM_tD7Q4rGpxIHNKAeQVUrle_PYI_sdFM").build()
@@ -34,7 +82,12 @@ conv_handler = ConversationHandler(
     fallbacks=[],
 
     states={
-        0: [CommandHandler('name', add_name)],
+        ADD_NAME: [MessageHandler(filters.TEXT, add_name)],
+        ADD_TYPE: [MessageHandler(filters.TEXT, add_type)],
+        ADD_DATE: [MessageHandler(filters.TEXT, add_date)],
+        ADD_LENGHT: [MessageHandler(filters.TEXT, add_lenght)],
+        ADD_MEMBERS: [MessageHandler(filters.TEXT, add_members)],
+        ADD_RISK: [MessageHandler(filters.TEXT, add_risk)],
     },
 )
 app.add_handler(conv_handler)
